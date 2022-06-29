e28d5ccd4cfbb0947c133b9ed0c656f8c3c90679
Tristan Campbell <tristan.campbell@intel.com>
2022-06-29 06:28:56 -0700

    Intial commit - iface update

---
+++ src/metadata/metadata_raw_pmem.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright(c) 2012-2021 Intel Corporation
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef __METADATA_RAW_PMEM_H__
+#define __METADATA_RAW_PMEM_H__
+
+int _raw_ram_init_pmem(ocf_cache_t cache,
+	ocf_flush_page_synch_t lock_page_pfn,
+	ocf_flush_page_synch_t unlock_page_pfn,
+	struct ocf_metadata_raw *raw);
+
+
+int _raw_ram_deinit_pmem(ocf_cache_t cache,
+    struct ocf_metadata_raw *raw);
